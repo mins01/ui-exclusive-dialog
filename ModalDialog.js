fca96e7adfeb07a9ec3132f4ddecb18a337d6784
mins01 <mins01.lycos.co.kr@gmail.com>
2025-08-05 17:57:08 +0900

    계속

--- ModalDialog.js
+++ ModalDialog.js
@@ -12,19 +12,52 @@ class ModalDialog{
             confirm:this.container.querySelector('.modal-dialog-confirm'),
             prompt:this.container.querySelector('.modal-dialog-prompt'),
         }
-        console.log(this.dialogs.alert);
-        this.dialogs.alert.addEventListener('cancel',(event)=>{alert('2'); event.preventDefault();})
         if(this.dialogs.alert){ 
-            this.dialogs.alert.addEventListener('cancel',(event)=>{event.preventDefault();}) 
-            this.dialogs.alert.closedBy="none";
+            const dialog = this.dialogs.alert
+            dialog.closedBy="none"; // 크롬대응
+            dialog.addEventListener('cancel',(event)=>{event.preventDefault();})  // 사파리 대응. ESC 키 막기
+            dialog.addEventListener('close',(event)=>{ console.log('close');
+                const target = event.target
+                this.running = false;
+                this.runQueue()
+                console.log('returnValue',target.returnValue);
+                
+                if(target._resolve){
+                    target._resolve(target.returnValue);
+                }
+            })
         }
         if(this.dialogs.confirm){ 
-            this.dialogs.confirm.closedBy="none";
-            this.dialogs.confirm.addEventListener('cancel',(event)=>{event.preventDefault();}) 
+            const dialog = this.dialogs.confirm
+            dialog.closedBy="none"; // 크롬대응
+            dialog.addEventListener('cancel',(event)=>{event.preventDefault();})  // 사파리 대응. ESC 키 막기
+            dialog.addEventListener('close',(event)=>{ console.log('close');
+                const target = event.target
+                this.running = false;
+                this.runQueue()
+                console.log('returnValue',target.returnValue);
+                if(target._resolve){
+                    target._resolve(target.returnValue==='true');
+                }
+            }) 
         }
         if(this.dialogs.prompt){ 
-            this.dialogs.confirm.closedBy="none";
-            this.dialogs.prompt.addEventListener('cancel',(event)=>{event.preventDefault();}) 
+            const dialog = this.dialogs.prompt
+            dialog.closedBy="none"; // 크롬대응
+            dialog.addEventListener('cancel',(event)=>{event.preventDefault();})  // 사파리 대응. ESC 키 막기
+            dialog.addEventListener('close',(event)=>{ console.log('close');
+                const target = event.target
+                this.running = false;
+                this.runQueue()
+                console.log('returnValue',target.returnValue);
+                if(target._resolve){
+                    if(target.returnValue==='true'){
+                        target._resolve(dialog.querySelector('.modal-dialog-input')?.value);
+                    }else{
+                        target._resolve(null);
+                    }
+                }
+            }) 
         }
         this.queue = [];
         this.removeEventHandlers = [];
@@ -35,116 +68,35 @@ class ModalDialog{
     }
 
     alert(message=''){
-        const promise = new Promise((resolve,reject)=>{ this.addQueue(()=>{this.showAlert(message,resolve,reject)}) })
-        if(!this.running) this.runQueue();
+        const promise = new Promise((resolve,reject)=>{ this.addQueue(()=>{this.showModal(resolve,reject,this.dialogs.alert,message)}) })
+        this.runQueue();
         return promise;
-    }
-    showAlert(message='',resolve,reject){
-        console.log('showAlert',[...arguments]);
-        
-        this.runRemoveEventHandlers();
-        const dialog = this.dialogs.alert
-        const messageBox = dialog.querySelector('.modal-dialog-message');
-        messageBox.textContent = message;
-        
-        const btnConfirm = dialog.querySelector('.modal-dialog-btn-confirm');
-        if(btnConfirm){
-            const fnClick = ()=>{
-                if(resolve) resolve();
-                dialog.close();
-                this.running = false;
-                this.runQueue(); // 다음 큐 실행
-            };
-            btnConfirm.addEventListener('click',fnClick,{once:true})
-            this.removeEventHandlers.push(()=>{ btnConfirm.removeEventListener('click',fnClick) }) // 이벤트 제거 등록
-        }
-        dialog.showModal();
     }
     confirm(message=''){
-        const promise = new Promise((resolve,reject)=>{ this.addQueue(()=>{this.showConfirm(message,resolve,reject)}) })
-        if(!this.running) this.runQueue();
+        const promise = new Promise((resolve,reject)=>{ this.addQueue(()=>{this.showModal(resolve,reject,this.dialogs.confirm,message)}) })
+        this.runQueue();
         return promise;
     }
-    showConfirm(message='',resolve,reject){
-        console.log('showConfirm',[...arguments]);
-        
-        this.runRemoveEventHandlers();
-        const dialog = this.dialogs.confirm
-        const messageBox = dialog.querySelector('.modal-dialog-message');
-        messageBox.textContent = message;
-
-        const btnConfirm = dialog.querySelector('.modal-dialog-btn-confirm');
-        if(btnConfirm){
-            const fnClick = ()=>{
-                if(resolve) resolve(true);
-                dialog.close();
-                this.running = false;
-                this.runQueue(); // 다음 큐 실행
-            };
-            btnConfirm.addEventListener('click',fnClick,{once:true})
-            this.removeEventHandlers.push(()=>{ btnConfirm.removeEventListener('click',fnClick) }) // 이벤트 제거 등록
-        }
-
-        const btnConcel = dialog.querySelector('.modal-dialog-btn-cancel');
-        if(btnConcel){
-            const fnClick = ()=>{
-                if(resolve) resolve(false);
-                dialog.close();
-                this.running = false;
-                this.runQueue(); // 다음 큐 실행
-            };
-            btnConcel.addEventListener('click',fnClick,{once:true})
-            this.removeEventHandlers.push(()=>{ btnConcel.removeEventListener('click',fnClick) }) // 이벤트 제거 등록
-        }
-        
-
-        dialog.showModal();
-    }
+    
     prompt(message='',defaultValue=''){
-        const promise = new Promise((resolve,reject)=>{ this.addQueue(()=>{this.showPrompt(message,defaultValue,resolve,reject)}) })
-        if(!this.running) this.runQueue();
+        const promise = new Promise((resolve,reject)=>{ this.addQueue(()=>{this.showModal(resolve,reject,this.dialogs.prompt,message,defaultValue)}) })
+        this.runQueue();
         return promise;
     }
-    showPrompt(message='',defaultValue='',resolve,reject){
+    
+    showModal(resolve,reject,dialog,message='',defaultValue=''){
         console.log('showAlert',[...arguments]);
-        
-        this.runRemoveEventHandlers();
-        const dialog = this.dialogs.prompt
+        dialog._resolve = resolve
+        dialog._reject = reject
         const messageBox = dialog.querySelector('.modal-dialog-message');
-        messageBox.textContent = message;
-        
+        if(messageBox){ messageBox.textContent = message; }
         const input = dialog.querySelector('.modal-dialog-input');
         if(input){
             input.defaultValue = defaultValue
             input.value = defaultValue
         }
-
-        const btnConfirm = dialog.querySelector('.modal-dialog-btn-confirm');
-        if(btnConfirm){
-            const fnClick = ()=>{
-                if(resolve) resolve(input?.value);
-                dialog.close();
-                this.running = false;
-                this.runQueue(); // 다음 큐 실행
-            };
-            btnConfirm.addEventListener('click',fnClick,{once:true})
-            this.removeEventHandlers.push(()=>{ btnConfirm.removeEventListener('click',fnClick) }) // 이벤트 제거 등록
-        }
-        const btnConcel = dialog.querySelector('.modal-dialog-btn-cancel');
-        if(btnConcel){
-            const fnClick = ()=>{
-                if(resolve) resolve();
-                dialog.close();
-                this.running = false;
-                this.runQueue(); // 다음 큐 실행
-            };
-            btnConcel.addEventListener('click',fnClick,{once:true})
-            this.removeEventHandlers.push(()=>{ btnConcel.removeEventListener('click',fnClick) }) // 이벤트 제거 등록
-        }
         dialog.showModal();
     }
-
-
 
 
     addQueue(q){
